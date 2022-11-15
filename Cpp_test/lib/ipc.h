830b54d598bfc10faaf9a8785f66900f1774b7dd
Giang Le <giang.le@unikie.com>
2022-11-15 16:07:28 +0200

    Rename ipc-common.h to ipc.h

---
+++ Cpp_test/lib/ipc.h
@@ -0,0 +1,42 @@
+#ifndef IPC_H_
+#define IPC_H_
+
+#include <iostream>
+#include <utility>
+
+enum class IPCProtocol
+{
+	NONE, QUEUE, HELP, PIPE, SHARE, 
+};
+
+
+enum class IPCMode
+{
+	SEND_MODE, RECEIVE_MODE
+};
+
+
+struct ipc_info{
+    IPCMode mode{};
+    IPCProtocol protocol{};
+	char *file_name;
+    char *method_name;
+};
+
+
+class IPC
+{   
+    protected:
+        ipc_info info{};
+
+	public:
+
+        explicit IPC(ipc_info options) : info(std::move(options))
+		{};
+		virtual void init() = 0;
+		virtual void transfer() = 0;
+		 ~IPC() = default;
+};
+
+
+#endif
