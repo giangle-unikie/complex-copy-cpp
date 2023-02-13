f19b3dea8fcdd94d4a91141bc18774e682001aa6
Giang Le <giang.le@unikie.com>
2023-02-13 16:26:13 +0200

    Add input() and empty line between functions

--- python/sort_number.py
+++ python/sort_number.py
@@ -1,21 +1,22 @@
 import sys
+
+
 #This function will sort a given sequence of numbers
 #if value in the list of numbers is not interger or float, program will exit
 def sort_numbers(numbers):
     for value in numbers:
-        check_value_int = isinstance(value, int)
-        check_value_float = isinstance(value, float)
-
-        if not check_value_int and not check_value_float:
-            print("Error: " + str(value) + " is not integer or float.")
+        check_value_number = value.replace('.', '', 1).isdigit()
+        if not check_value_number:
+            print("Error: " ,value, " is not integer or float.")
             sys.exit(1)
 
     numbers.sort()
     return numbers
 
+
 def main():
-    numbers = [3.9, 1.16, 4, 111, 5, 9, 2, 6, 5, 3, 5.0001]
-    print(sort_numbers(numbers))
+    numbers_list = input("Enter the numbers separated by space: ").strip().split()
+    print(sort_numbers(numbers_list))
 
 
 if __name__ == "__main__":
