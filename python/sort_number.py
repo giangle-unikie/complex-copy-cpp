4be04da8812db390389f999dea167b70ed65c89d
Giang Le <giang.le@unikie.com>
2023-02-13 17:40:05 +0200

    Change variable name and add negative numbers solution

--- python/sort_number.py
+++ python/sort_number.py
@@ -5,9 +5,9 @@ import sys
 #if value in the list of numbers is not interger or float, program will exit
 def sort_numbers(numbers):
     for value in numbers:
-        check_value_number = value.replace('.', '', 1).isdigit()
-        if not check_value_number:
-            print("Error: " ,value, " is not integer or float.")
+        value_is_number = value.replace('.', '', 1).isdigit()
+        if not value_is_number and not (value.startswith("-") and value[1:].isdigit()):
+            print("Error:", value, "is not integer or float.")
             sys.exit(1)
 
     numbers.sort()
